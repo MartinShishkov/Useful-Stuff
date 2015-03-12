360b1197e4329071a4576c4254eb1844421275e5
Martin Shishkov <martin.shishkov@hotmail.com>
2015-03-12 02:09:48 +0200

    CKEditor with user friendly TeX commands insert and live preview.

---
+++ MathKeyboard_CKEDITOR/Init.js
@@ -0,0 +1,25 @@
+﻿CKEDITOR.config.width = 500;
+
+function InsertText(text) {
+    // Get the editor instance that we want to interact with.
+    var editor = CKEDITOR.instances.MathInput;
+    // Check the active editing mode.
+    if (editor.mode == 'wysiwyg') {
+        editor.insertText(text);
+    }
+    else
+        alert('You must be in WYSIWYG mode!');
+}
+
+function GetContents() {
+    var editor = CKEDITOR.instances.MathInput;
+    return editor.getData();
+}
+
+function onFocus() {
+    $('#math-table').show();
+}
+
+function onBlur() {
+    $('#math-table').hide();
+}
---
+++ MathKeyboard_CKEDITOR/custom-script.js
@@ -0,0 +1,57 @@
+﻿(function () {
+    var keyboardButtons = [
+        "\\frac{a}{b}",
+        "\\sqrt{a}",
+        "\\sqrt[b]{a}",
+        "\\sqrt{\\frac{a}{b}}",
+        "a^{bb}",
+        "a_{bb}",
+        "\\alpha",
+       "\\beta",
+        "\\gamma",
+        "\\pi",
+        "\\infty"
+    ];
+
+    // creates a table which holds the buttons
+    // the buttons' value is the according math symbol
+    // on button click the TeX command is inserted
+    // at the current cursor position in the text editor
+
+    var table = $("#math-table");
+
+    table.append("<tr>");
+    for (var i = 0; i < keyboardButtons.length; i++) {
+
+        // we need '`' signs aroung the value 
+        // in order for the equation to render properly
+        // by mathjax
+        var val = "`" + keyboardButtons[i] + "`";
+
+        // creating the button
+        var btn = $("<td><button>" + val + "</button></td>");
+
+        // attaching the value to the button in
+        // order to get it later
+        $(btn).data("eq", val);
+
+        // button click inserts button's value
+        // at the current cursor position in the editor
+        // and updates the preview div's html
+        // calling MathJax.Hub.Queue to make MathJax process the page again
+        // in order to render the new mathematics
+        // and see the end result
+        $(btn).on('click', function () {
+            InsertText($(this).data("eq"));
+            console.log(GetContents());
+            $('#MathOutput').html(GetContents());
+            MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+        });
+
+        table.append(btn);
+    }
+    table.append("</tr>");
+
+    $(table).hide();
+    
+}());
